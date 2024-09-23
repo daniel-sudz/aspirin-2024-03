56a3d3b7c0a61e7d44c46badfd2f78d091f6bd0c
Daniel Sudzilouski <daniel@sudzilouski.com>
2024-09-23 18:35:19 -0400

    implement double linkedlist

--- assignments/01-ownership-borrowing/src/doubly_linked_list.rs
+++ assignments/01-ownership-borrowing/src/doubly_linked_list.rs
@@ -16,14 +16,58 @@ pub struct LinkedStack {
 
 impl LinkedStack {
     fn new() -> Self {
-        todo!();
+        LinkedStack {
+            head: None,
+            tail: None,
+        }
     }
 
     fn push(&mut self, val: i32) {
-        todo!()
+        match self.head.take() {
+            None => {
+                self.head = {
+                    Some(Box::new(Node {
+                        val,
+                        next: None,
+                        prev: None,
+                    }))
+                };
+            }
+            Some(mut prev_head ) => {
+                let mut next_head = Some(Box::new(Node {
+                    val,
+                    next: None,
+                    prev: Some(prev_head)
+                }));
+                // here we encounter an issue as we have a circular ownership problem
+                // the compiler returns "value partially assigned here after move"
+                prev_head.next = next_head.take();
+                self.head = next_head.take();
+            }
+        }
     }
 
     fn pop(&mut self) -> Option<i32> {
-        todo!();
+        match self.head.take() {
+            None => None,
+            Some(mut node) => {
+                // get value removed
+                let val: Option<i32> = Some(node.val);
+                // go back one
+                let prev = node.prev.take();
+                // set next of prev to None
+                let mut prev = match prev {
+                    None => None,
+                    Some(mut prev) => {
+                        prev.next = None;
+                        Some(prev)
+                    }
+                };
+                // move prev onto head
+                self.head = prev.take();
+                // return value
+                val
+            }
+        }
     }
 }
