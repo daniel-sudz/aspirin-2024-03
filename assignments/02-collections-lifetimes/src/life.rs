aa3a27dfa3629d38774d98b76455d8b6b79ecf50
Daniel Sudzilouski <daniel@sudzilouski.com>
2024-09-29 18:20:24 -0400

    extend lifetime in find_differences

--- assignments/02-collections-lifetimes/src/life.rs
+++ assignments/02-collections-lifetimes/src/life.rs
@@ -10,18 +10,18 @@ fn split_string<'a>(string: &'a str, delimeter: &str) -> Vec<&'a str> {
 }
 
 #[derive(PartialEq, Debug)]
-struct Differences<'a> {
+struct Differences<'a,'b> {
     only_in_first: Vec<&'a str>,
-    only_in_second: Vec<&'a str>,
+    only_in_second: Vec<&'b str>,
 }
 
 // re-use the split_string function and use the built in contains to match on every possible word
-fn find_differences<'a>(first_string: &'a str, second_string: &'a str) -> Differences<'a> {
+fn find_differences<'a,'b>(first_string: &'a str, second_string: &'b str) -> Differences<'a,'b> {
     let split_first = split_string(first_string, " ");
     let split_second = split_string(second_string, " ");
 
     let mut only_in_first: Vec<&'a str> = Vec::new();
-    let mut only_in_second: Vec<&'a str> = Vec::new();
+    let mut only_in_second: Vec<&'b str> = Vec::new();
 
     for word in &split_first {
         if !second_string.contains(word) {
