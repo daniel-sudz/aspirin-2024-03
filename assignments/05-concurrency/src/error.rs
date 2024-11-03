dd15056ad2a7e303ead4100b7d18ab84e64c872e
Daniel Sudzilouski <daniel@sudzilouski.com>
2024-11-03 01:28:52 -0400

    cleanup template

--- assignments/05-concurrency/src/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Error, Debug, PartialEq)]
-pub enum ThreadPoolError {
-    #[error("Number of threads must be greater than 0")]
-    ZeroThreads,
-    #[error("Failed to send job to worker")]
-    Send,
-}
