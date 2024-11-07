80fc3b5d186b07e22612bde8791884ac3615d314
Daniel Sudzilouski <daniel@sudzilouski.com>
2024-11-07 14:06:13 -0500

    finish list ports

--- mini-projects/01-device-driver/laptop-interface/src/list_ports.rs
+++ mini-projects/01-device-driver/laptop-interface/src/list_ports.rs
@@ -31,3 +31,50 @@ extern "C" {
     // void sp_free_port_list(struct sp_port **port_list);
     fn sp_free_port_list(port_list: *mut *mut sp_port);
 }
+
+// Example output: 
+// ---- list_ports::tests::test_list_ports stdout ----
+// Port name: /dev/cu.Bluetooth-Incoming-Port
+// Port name: /dev/cu.usbmodem2101
+pub fn list_ports() -> Vec<String> {
+    unsafe {
+        let mut port_list: *mut *mut sp_port = ptr::null_mut();
+        let result: SpReturn = sp_list_ports(&mut port_list);
+
+        let mut ports: Vec<String> = Vec::new();
+
+        match result {
+            SpReturn::SP_OK => {
+                let mut i = 0;
+                while !(*port_list.add(i)).is_null() { 
+                    let port = *port_list.add(i);
+                    let port_name = sp_get_port_name(port);
+                    if !port_name.is_null() {
+                        let port_name_str = CStr::from_ptr(port_name).to_string_lossy();
+                        ports.push(port_name_str.to_string());
+                        println!("Port name: {}", port_name_str);
+                    }
+                    i += 1;
+                }
+
+                // Don't forget to free the port list when done
+                sp_free_port_list(port_list);
+            }
+            _ => {
+                println!("Error: {:?}", result);
+            }
+        }
+        ports
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_list_ports() {
+        list_ports();
+    }
+}
--- mini-projects/01-device-driver/laptop-interface/src/main.rs
+++ mini-projects/01-device-driver/laptop-interface/src/main.rs
@@ -1,3 +1,4 @@
+mod list_ports;
 fn main() {
     println!("Hello, World!");
 }
