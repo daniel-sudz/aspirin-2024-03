2d17784c5d43d7c8a693fcb8ba408d3c4a20b37f
Rixhee <rishitbansals@gmail.com>
2024-11-15 03:10:12 -0500

    Basic gui
    Wrote gui.rs with the basic gui using egui with various functions
    to display various states of the game

---
+++ mini-projects/01-device-driver/laptop-interface/src/gui.rs
@@ -0,0 +1,151 @@
+use eframe::egui;
+
+#[derive(PartialEq)]
+enum DeviceState {
+    PendingInit = 0,
+    PendingStart = 1,
+    Running = 2,
+    Complete = 3,
+}
+
+pub struct GameApp {
+    controller_one_pos: (i32, i32),
+    controller_two_pos: (i32, i32),
+    state: DeviceState,
+    winner: Option<String>,
+}
+
+impl Default for GameApp {
+    fn default() -> Self {
+        Self {
+            controller_one_pos: (0, 0),
+            controller_two_pos: (0, 0),
+            state: DeviceState::PendingInit,
+            winner: None,
+        }
+    }
+}
+impl eframe::App for GameApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| match self.state {
+            DeviceState::PendingInit => {
+                display_welcome(ui);
+                if ctx.input(|input| input.key_pressed(egui::Key::Enter)) {
+                    self.state = DeviceState::PendingStart;
+                }
+            }
+            DeviceState::PendingStart => {
+                display_starting(ui);
+                if ctx.input(|input| input.key_pressed(egui::Key::Enter)) {
+                    self.state = DeviceState::Running;
+                }
+            }
+            DeviceState::Running => {
+                display_running(ui, self.controller_one_pos, self.controller_two_pos);
+                if ctx.input(|input| input.key_pressed(egui::Key::Space)) {
+                    self.state = DeviceState::Complete;
+                }
+            }
+            DeviceState::Complete => {
+                display_complete(ui, &self.winner);
+                if ctx.input(|input| input.key_pressed(egui::Key::Enter)) {
+                    self.state = DeviceState::PendingStart;
+                } else if ctx.input(|input| input.key_pressed(egui::Key::Space)) {
+                    self.state = DeviceState::PendingInit;
+                }
+            }
+        });
+    }
+}
+
+fn display_welcome(ui: &mut egui::Ui) {
+    ui.centered_and_justified(|ui| {
+        ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
+            ui.add(egui::Label::new(
+                egui::RichText::new("Welcome").size(40.0).strong(),
+            ));
+            ui.add(egui::Label::new(
+                egui::RichText::new("Initializing controller 0 and 1...")
+                    .size(20.0)
+                    .italics(),
+            ));
+        });
+    });
+}
+
+fn display_starting(ui: &mut egui::Ui) {
+    ui.centered_and_justified(|ui| {
+        ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
+            ui.add(egui::Label::new(
+                egui::RichText::new("Welcome").size(40.0).strong(),
+            ));
+            ui.add(egui::Label::new(
+                egui::RichText::new("Initialized controller 0 and 1...")
+                    .size(20.0)
+                    .italics(),
+            ));
+            ui.add_space(20.0);
+            ui.add(egui::Label::new(
+                egui::RichText::new("Press Enter to start")
+                    .size(20.0)
+                    .strong()
+                    .italics(),
+            ));
+        });
+    });
+}
+
+fn display_running(
+    ui: &mut egui::Ui,
+    controller_one_pos: (i32, i32),
+    controller_two_pos: (i32, i32),
+) {
+    ui.vertical_centered(|ui| {
+        egui::Grid::new("controllers_grid")
+            .num_columns(2)
+            .spacing([50.0, 50.0])
+            .show(ui, |ui| {
+                ui.add(egui::Label::new(
+                    egui::RichText::new("Controller 0").size(40.0).strong(),
+                ));
+                ui.add(egui::Label::new(
+                    egui::RichText::new("Controller 1").size(40.0).strong(),
+                ));
+                ui.end_row();
+
+                ui.add(egui::Label::new(
+                    egui::RichText::new(format!("{:?}", controller_one_pos)).size(40.0),
+                ));
+                ui.add(egui::Label::new(
+                    egui::RichText::new(format!("{:?}", controller_two_pos)).size(40.0),
+                ));
+                ui.end_row();
+            });
+    });
+}
+
+fn display_complete(ui: &mut egui::Ui, winner: &Option<String>) {
+    ui.centered_and_justified(|ui| {
+        ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
+            ui.add(egui::Label::new(
+                egui::RichText::new("Game complete").size(40.0).strong(),
+            ));
+            ui.add(egui::Label::new(
+                egui::RichText::new(format!("The winner is {:?}", *winner)).size(20.0),
+            ));
+            ui.add_space(20.0);
+            ui.add(egui::Label::new(
+                egui::RichText::new("Press Enter to restart")
+                    .size(20.0)
+                    .strong()
+                    .italics(),
+            ));
+            ui.add(egui::Label::new(
+                egui::RichText::new("Press Space to reset")
+                    .size(20.0)
+                    .strong()
+                    .italics(),
+            ));
+        });
+    });
+}
--- mini-projects/01-device-driver/laptop-interface/src/main.rs
+++ mini-projects/01-device-driver/laptop-interface/src/main.rs
@@ -1,6 +1,18 @@
-mod libserial;
 mod commander;
+mod controller_management;
+mod gui;
+mod libserial;
+mod threads;
 
-fn main() {
-    println!("Hello, World!");
+use eframe::NativeOptions;
+use gui::GameApp;
+
+fn main() -> Result<(), eframe::Error> {
+    eframe::run_native(
+        "Mini Project 1",
+        NativeOptions::default(),
+        Box::new(|_cc| Ok(Box::new(GameApp::default()))),
+    );
+
+    Ok(())
 }
